7ee878a824301cca7b897badcafd0ec77c53a78e
gpucce <g.puccetti92@gmail.com>
2024-10-25 14:23:29 +0200

    clean slurm

--- finetuning/finetune_summary-lora-multigpu.sh
+++ finetuning/finetune_summary-lora-multigpu.sh
@@ -1,20 +1,3 @@
-#!/bin/bash
-#SBATCH --job-name=finetuning_summary_mistral_base-lora            # Job name
-#SBATCH --output /leonardo_scratch/large/userexternal/gpuccett/Repos/minerva_sft/slurm_logs/sft_%j.out       # Name of stdout output file
-#SBATCH --nodes=1                       # number of nodes
-#SBATCH --ntasks-per-node=1             # number of tasks per node
-#SBATCH --cpus-per-task=32              # number of threads per task
-#SBATCH --time 12:00:00                 # format: HH:MM:SS
-#SBATCH --gres=gpu:4                    # number of gpus per node
-#SBATCH -A EUHPC_E03_068
-#SBATCH -p boost_usr_prod
-
-module load profile/deeplrn
-module load openmpi/4.1.4--gcc--11.3.0-cuda-11.8 zlib/1.2.13--gcc--11.3.0 cuda/11.8
-
-eval "$(conda shell.bash hook)" # init conda
-conda activate /leonardo_scratch/large/userexternal/gpuccett/Repos/minerva_sft/conda_venv
-
 # export OMP_PROC_BIND=true
 export WANDB_MODE=offline
 
