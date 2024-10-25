1a6daf9c0e12d9a8ca38abc42af6ec0bdfcc7cc0
gpucce <g.puccetti92@gmail.com>
2024-10-25 14:24:33 +0200

    clean slurm

--- finetuning/finetune_summary-lora-multigpu.sh
+++ finetuning/finetune_summary-lora-multigpu.sh
@@ -1,8 +1,7 @@
 # export OMP_PROC_BIND=true
 export WANDB_MODE=offline
 
-accelerate launch --config_file=/leonardo_scratch/large/userexternal/gpuccett/Repos/minerva_sft/accelerate_configurations/fsdp_lora.yaml \
+accelerate launch --config_file=./accelerate_configurations/fsdp_lora.yaml \
     --num_processes 4 \
-    /leonardo_scratch/large/userexternal/gpuccett/Repos/minerva_sft/finetuning/finetune_summary.py \
+    ./minerva_sft/finetuning/finetune_summary.py \
     -c $1
-    #-c /leonardo/home/userexternal/lmoroni0/__Work/minerva_sft/configurations/mistral-base_continual-lora.yaml
